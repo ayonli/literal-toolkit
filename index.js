57fcb259d80d02a890196929e353e995c49e9020
Ayon Lee <i@hyurl.com>
2018-12-12 23:45:31 +0800

    add regexp and comment support

--- index.js
+++ index.js
@@ -4,3 +4,5 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.string = require("./lib/string");
 exports.number = require("./lib/number");
 exports.constant = require("./lib/constant");
+exports.regexp = require("./lib/regexp");
+exports.comment = require("./lib/comment");
